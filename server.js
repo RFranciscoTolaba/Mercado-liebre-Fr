25257d296ac0d9d80c123fd0cc8c2f531cbbd4cb
FranciscoTolaba <franciscotolaba66@gmail.com>
2023-08-23 19:47:05 -0300

    ulitmos cambios json

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'))
 
-app.listen(3000, () =>{
-    console.log('corriendo servidor');
-    })
+app.listen(PORT, () =>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/index.html'))
